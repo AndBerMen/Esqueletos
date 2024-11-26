b86079ab29330bbfcb2cb4a82c381a17c15cf8bb
JavierCornejoLeal <cornejolealjavier@gmail.com>
2024-11-26 23:18:33 +0100

    Cambios en Posts, Comentaris i Etiquetes

---
+++ blog/tag/alta/altaEtiqueta.js
@@ -0,0 +1,21 @@
+window.onload = main;
+
+function main() {
+    document.getElementById("botonAlta").addEventListener("click", alta);
+    document.getElementById("botonTornar").addEventListener("click", tornar);
+
+    let boton = document.getElementById("dropdown");
+    let menu  = document.querySelector("nav");
+
+    boton.addEventListener("click", () => {
+        menu.classList.toggle('show');
+    });
+}
+
+function alta(){
+    window.location.assign("../llistat/llistarEtiquetes.html");
+}
+
+function tornar(){
+    window.location.assign("../llistat/llistarEtiquetes.html");
+}
